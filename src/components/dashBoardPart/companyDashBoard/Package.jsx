596234182202ccee5375ea389fbce2349b5aaa5d
Agbo-Emmanuel <agboe4102@gmail.com>
2024-03-04 10:46:48 +0100

    jgt

--- src/components/dashBoardPart/companyDashBoard/Package.jsx
+++ src/components/dashBoardPart/companyDashBoard/Package.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import axios from 'axios'
 import '../companyDashBoard/companyDashboardCss/package.css'
+import { Thecontext } from '../../../App'
+import { Link } from 'react-router-dom'
 
 
 
 
 const Package = () => {
+
+
+  const url = 'https://fakestoreapi.com/products'
+
+    const [packages, setPackages] = useState([])
+    // const {dispatch, setAmount} = useContext(Thecontext)
+
+
+    useEffect(()=>{
+        axios.get(url)
+            .then((res) => {
+              setPackages(res.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
 
 
   return (
@@ -14,7 +34,7 @@ const Package = () => {
       <div className='PackagePage'>
         <div className='packagePageLeft'>
           <div className='packagePageLeftTop'>
-            List of Packages
+            List of Unassigned Packages
           </div>
           <div className='packagePageLeftLine'></div>
           <div className='packageSearchPart'>
@@ -27,15 +47,77 @@ const Package = () => {
           </div>
           <div className='packagePageLeftLine'></div>
           <div className='listOfPackages'>
-            <div></div>
+            <div className='packageListProperties'>
+              <nav>name</nav>
+              <nav>ID</nav>
+            </div>
+            <div className='packagePageLeftLine'></div>
+            <div className='thePackages'>
+              {
+                packages.map(()=>{
+                  return(
+                    <div className='thePackagesItems'>
+                      <p>Shoe</p>
+                      <p>23495</p>
+                    </div>
+                  )
+                })
+              }
+            </div>
           </div>
         </div>
         <div className='packagePageRight'>
           <div className='packagePageRightTop'>
-
+            <nav>List of Assigned Packages</nav>
+          </div>
+          <div className='packagePageRightLine'></div>
+          <div className='packageSearchPart'>
+            <input 
+              type='text'
+              placeholder='search ID'
+              className='searchInput'
+            />
+            <button className='searchBtn'>Search</button>
+          </div>
+          <div className='packagePageRightLine'></div>
+          <div className='listOfPackages'>
+            <div className='packageListProperties'>
+              <nav>name</nav>
+              <nav>ID</nav>
+            </div>
+            <div className='packagePageLeftLine'></div>
+            <div className='thePackages'>
+              {/* <div className='thePackagesItems'>
+                <p>Shoe</p>
+                <p>23495</p>
+              </div>
+              <div className='thePackagesItems'>
+                <p>Shoe</p>
+                <p>23495</p>
+              </div> */}
+            </div>
           </div>
         </div>
       </div>
+
+
+
+      {/* {
+    packages.map((product)=>{
+
+        return (
+
+            <div className='Product'>
+                <Link  to={`/detailpage/${product.id}`}  className='product'>
+                    <img src={product.image} alt=''/>
+                    <h2>{product.title}</h2>
+                    <p>price: ${product.price}</p>
+                </Link>
+            </div>
+        )
+
+    })
+} */}
 
     </>
   )
